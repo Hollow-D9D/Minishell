79b7d651ba0384fa8e10e864b005463a45032243
TryToFly <tharutyu@c1r1s3.42yerevan.am>
2021-05-11 15:07:13 +0400

    libft added

---
+++ libft/get_next_line.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tharutyu <tharutyu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 02:00:55 by tharutyu          #+#    #+#             */
+/*   Updated: 2021/04/27 02:01:32 by tharutyu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_get_next_line(int fd, char **line)
+{
+	int		i;
+	int		n;
+	char	c;
+	char	*str;
+
+	i = 0;
+	if (fd < 0)
+		return (-1);
+	if (!(str = (char*)malloc(1001)))
+		return (-1);
+	while ((n = read(fd, &c, 1)) && c != '\n' && c != '\0')
+	{
+		if (c != '\n' && c != '\0')
+			str[i] = c;
+		++i;
+	}
+	str[i] = '\0';
+	*line = str;
+	return (n);
+}
